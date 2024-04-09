ac8751145aeee10fbc0bc8402d7c4af4e1ad1cdf
王杰 <jie985563349@outlook.com>
2024-04-09 16:15:11 +0800

    event handle

--- App.jsx
+++ App.jsx
@@ -1,14 +1,22 @@
 import React from './core/react.js';
 
 function Counter({ num }) {
-  return <div>count: {num}</div>;
+  const handleClick = () => {
+    console.log('click');
+  };
+
+  return (
+    <div>
+      count: {num}
+      <button onClick={handleClick}>click</button>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
       hello react
-      <Counter num={10} />
       <Counter num={10} />
     </div>
   );
